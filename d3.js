03796e61809d91e06bcafc4da3a8b435345d3ef3
elliejy <tachidita@gmail.com>
2019-07-19 10:39:00 -0700

    Include d3 packages

--- d3.js
+++ d3.js
@@ -1,5 +1,14 @@
 import * as d3 from 'd3';
+import { select } from "d3-selection";
+import { geomap } from "d3-geomap";
+
 
 d3.csv( "/WeatherData.csv").then((d) => {
     console.log( d[0] )
 });
+
+// var map = d3
+//   .geomap()
+//   .geofile("/d3-geomap/topojson/world/countries.json")
+//   .draw(d3.select("#map"));
+
