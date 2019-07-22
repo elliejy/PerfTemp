4d9262fb17da5b36381a1c4e52bddbb28ef9f6e7
elliejy <tachidita@gmail.com>
2019-07-21 19:19:50 -0700

    Filter cities based on temp input

--- d3.js
+++ d3.js
@@ -1,7 +1,8 @@
 
 import { geomap } from "d3-geomap";
 import { select } from "d3-selection";
-import {draw, csv} from 'd3';
+import { draw, csv, geoEquirectangular, geoPath, geoMercator, geoStereographic} from 'd3';
+import queryString from 'query-string';
 
 
 csv( "/WeatherData.csv").then((d) => {
@@ -9,62 +10,128 @@ csv( "/WeatherData.csv").then((d) => {
 });
 
 document.addEventListener( 'DOMContentLoaded', ()=> {
-    // var map = geomap()
-    // map.geofile("./assets/data/countries.json")
-    // map.draw(select("#map"));
 
-var svg = d3.select( "svg" );
+var width = 1060,
+    height = 600;
 
-var path = d3.geoPath();
+    var projection = geoEquirectangular()
+   .scale(178 )
+    .translate( [width / 2, height / 2] )
+    .precision( .1 );
 
-    d3.json( "https://d3js.org/world-110m.v1.json", function ( error, us ) {
+var path = geoPath()
+    .projection( projection );
+
+var svg = select( "body" ).append( "svg" )
+    .attr( "width", width )
+    .attr( "height", height );
+
+var url = "https://d3js.org/world-110m.v1.json";
+var url2 = "/ne_50m_populated_places_simple.json";
+var url3 = "/WeatherData.csv";
+
+const query = queryString.parse(location.search);
+
+const queryIsValid = (
+    query.temperature && query.month && query.unit
+)
+
+d3.json(url, function ( error, world ) {
     if ( error ) throw error;
+    d3.json( url2, function ( err, places_geojson ) {
+        d3.csv( url3, function ( err, temp ) {
+        svg.append( "g" )
+            .attr( "class", "land" )
+            .selectAll( "path" )
+            .data( topojson.feature( world, world.objects.countries ).features )
+            .enter().append( "path" )
+            .attr( "d", path );
+        
+        svg.append( "path" )
+            .attr( "class", "boundary" )
+            .attr( "d", path (topojson.mesh( world, world.objects.countries, function ( a, b ) { return a !== b; } ) ));
+   
+        svg.selectAll( "text" )
+            .data( places_geojson.features)
+            .enter()
+            .append( "text" )
+            .text( function ( d ) {
+                if( queryIsValid ) {
+                    const tempData = temp.filter(city=> city.City === d.properties.name)
+                    if (tempData.length) {
+                        const cityMonth = tempData.filter(city => city.Month === query.month)
+                        if (cityMonth.length) {
+                            const city = cityMonth[0]
+                            if ( query.unit === 'celcius' ) {
+                               if (Math.abs( Number( query.temperature ) - Number( city.HighC ) ) <= 3)
+                                return city.City;
+                            } else {
+                                if( Math.abs( Number( query.temperature ) - Number( city.HighF ) ) <= 8)
+                                return city.City;
+                            }
+                        }
+                    } else {
+                        return "";
+                    }
+                }
+             })
+            .attr( "x", function ( d ) { return projection( d.geometry.coordinates )[0] ; } )
+            .attr( "y", function ( d ) { return projection( d.geometry.coordinates )[1] ; } )
+            .attr( "class", "labels" );
+            });
+            svg.selectAll( "circle" )
+            .data( places_geojson.features )
+            .enter()
+            .append( "circle" )
+            .attr( "r", function ( d ) {
+                console.log( d );
+                return d.properties.pop_max / 1000000;
+            } )
+           .attr( "cx", function ( d ) {
+               return projection( d.geometry.coordinates )[0]
+           } )
+            .attr( "cy", function ( d ) {
+                return projection( d.geometry.coordinates )[1]
+            } )
+            
+        });
+    });
 
-    svg.append( "g" )
-        .attr( "class", "states" )
-        .selectAll( "path" )
-        .data( topojson.feature( us, us.objects.countries ).features )
-        .enter().append( "path" )
-        .attr( "d", path )
 
-    svg.append( "path" )
-        .attr( "class", "state-borders" )
-        .attr( "d", path( topojson.mesh( us, us.objects.countries, function ( a, b ) { return a !== b; } ) ) );
+ 
+    select( self.frameElement ).style( "height", height + "px" );
+
 } );
-})
-// var width = 960,
-//     height = 600;
 
-// var projection = d3.geoConicEqualArea()
-//     .scale( 153 )
-//     .translate( [width / 2, height / 2] )
-//     .precision( .1 );
 
-// var path = d3.geoPath()
-//     .projection( projection );
+// const cityMonth = temp.filter( city => city.Month === query.month )
 
-// var graticule = d3.geoGraticule();
+// const tempCity = cityMonth.filter( city => {
+//     if ( query.unit === 'celcius' ) {
+//         return Math.abs( Number( query.temperature ) - Number( city.HighC ) ) <= 3
 
-// var svg = d3.select( "body" ).append( "svg" )
-//     .attr( "width", width )
-//     .attr( "height", height );
+//     } else {
+//         return Math.abs( Number( query.temperature ) - Number( city.HighF ) ) <= 8
+//     }
+// } )
 
-// d3.json( "data/world-110m.json", function ( error, world ) {
-//     if ( error ) throw error;
-//     console.log( world )
-//     console.log( topojson.feature( world, world.objects.land ) );
-//     console.log( graticule )
-//     svg.append( "path" )
-//         .datum( topojson.feature( world, world.objects.land ) )
-//         .attr( "class", "land" )
-//         .attr( "d", path );
-//     svg.append( "path" )
-//         .datum( topojson.mesh( world, world.objects.countries, function ( a, b ) { return a !== b; } ) )
-//         .attr( "class", "boundary" )
-//         .attr( "d", path );
-//     svg.append( "path" )
-//         .datum( graticule )
-//         .attr( "class", "graticule" )
-//         .attr( "d", path );
-// } );
-// d3.select( self.frameElement ).style( "height", height + "px" );
+// return tempCity.map( city => {
+//     if ( city.City === d.properties.name ) {
+//         return city
+//     }
+// } )
+        // svg.selectAll( "circle" )
+        //     .data( places_geojson.features )
+        //     .enter()
+        //     .append( "circle" )
+        //     .attr( "r", function ( d ) {
+        //         console.log( d );
+        //         return d.properties.pop_max / 1000000;
+        //     } )
+        //    .attr( "cx", function ( d ) {
+        //        return projection( d.geometry.coordinates )[0]
+        //    } )
+        //     .attr( "cy", function ( d ) {
+        //         return projection( d.geometry.coordinates )[1]
+        //     } )
+        // debugger
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-
+const d3 = require('d3');
+const fetch = require( 'fetch' );
 const express = require( "express" );
 const app = express();
 
-
 app.use( express.static( __dirname + '/public' ) );
-app.use("/assets/data", express.static(__dirname + '/assets/data'));
+// app.use("/assets/data", express.static(__dirname + '/assets/data'));
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+    
 const port = process.env.PORT || 5001;
 
 app.listen( port, () => { console.log( `Server is running on port ${ port }` ) } );
