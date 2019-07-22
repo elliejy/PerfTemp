3fd641881ecf70acce6d26fda6833506d7b2a3bf
elliejy <tachidita@gmail.com>
2019-07-21 22:15:57 -0700

    Add tooltips

--- d3.js
+++ d3.js
@@ -1,8 +1,13 @@
 
 import { geomap } from "d3-geomap";
 import { select } from "d3-selection";
-import { draw, csv, geoEquirectangular, geoPath, geoMercator, geoStereographic} from 'd3';
+import { draw, csv, geoEquirectangular, geoPath, event} from 'd3';
 import queryString from 'query-string';
+
+
+// csv( "/WeatherData.csv").then((d) => {
+//     // console.log( d[0] )
+// });
 
 document.addEventListener( 'DOMContentLoaded', ()=> {
 
@@ -22,10 +27,9 @@ var svg = select( "body" ).append( "svg" )
     .attr( "height", height )
     .attr("class", "cities")
 
-var div = select("body").append("div")
-    .attr("width", 150)
-    .attr("height", 600)
-    .attr( "display", "inline");
+ var div = d3.select( "body" ).append( "div" )
+        .attr( "class", "tooltip" )
+        .style( "opacity", 0 );
 
 const cityList = document.createElement("ul");
 document.body.appendChild(cityList);
@@ -48,6 +52,7 @@ d3.json(url, function ( error, world ) {
         places_geojson.features.map(d => {
             if ( queryIsValid ) {
                 let cityName = null;
+                let cityTemp = null;
                 const tempData = temp.filter( city => city.City === d.properties.name )
                 if ( tempData.length ) {
                     const cityMonth = tempData.filter( city => city.Month === query.month )
@@ -56,15 +61,18 @@ d3.json(url, function ( error, world ) {
                         if ( query.unit === 'celcius' ) {
                             if ( Math.abs( Number( query.temperature ) - Number( city.HighC ) ) <= 3 )
                                 cityName = city.City;
+                                cityTemp = city.HighC;
                         } else {
                             if ( Math.abs( Number( query.temperature ) - Number( city.HighF ) ) <= 8 )
                                 cityName = city.City;
+                                cityTemp = city.HighF;
+                                
                         }
                     }
                 }
                 if (cityName) {
                     const li = document.createElement( "li" )
-                    li.appendChild( document.createTextNode( d.properties.name ) );
+                    li.appendChild( document.createTextNode( d.properties.name + "[" +cityTemp+"]" ) );
                     cityList.append( li );
                 }
             }
@@ -75,6 +83,8 @@ d3.json(url, function ( error, world ) {
             .selectAll( "path" )
             .data( topojson.feature( world, world.objects.countries ).features )
             .enter().append( "path" )
+            .attr( "d", path )
+          
         
         svg.append( "path" )
             .attr( "class", "boundary" )
@@ -86,6 +96,7 @@ d3.json(url, function ( error, world ) {
         .append( "circle" )
         .attr( "r", function ( d ) {
             let cityName = null;
+            let cityTemp = null;
             const tempData = temp.filter( city => city.City === d.properties.name )
             if ( tempData.length ) {
                 const cityMonth = tempData.filter( city => city.Month === query.month )
@@ -94,14 +105,16 @@ d3.json(url, function ( error, world ) {
                     if ( query.unit === 'celcius' ) {
                         if ( Math.abs( Number( query.temperature ) - Number( city.HighC ) ) <= 3 )
                             cityName = city.City;
+                            cityTemp = city.HighC;
                     } else {
                         if ( Math.abs( Number( query.temperature ) - Number( city.HighF ) ) <= 8 )
                             cityName = city.City;
+                            cityTemp = city.HighF;
                     }
                 }
             }
             if (cityName) {
-                return 30;
+                return 5;
             } 
             return 0
         } )
@@ -111,19 +124,45 @@ d3.json(url, function ( error, world ) {
         .attr( "cy", function ( d ) {
             return projection( d.geometry.coordinates )[1]
         } )
-    })
+        .on( "mouseover", function ( d ) {
+            div.transition()
+                .duration( 200 )
+                .style( "opacity", .9 );
+            div.html( d.properties.name  )
+                .style( "left", ( event.pageX ) + "px" )
+                .style( "top", ( event.pageY - 28 ) + "px" );
+               
+        } )
+        .on( "mouseout", function ( d ) {
+            div.transition()
+                .duration( 500 )
+                .style( "opacity", 0 );
+        } );
+        })
         });
     });
-
-
  
     select( self.frameElement ).style( "height", height + "px" );
 
 } );
 
 
+// const cityMonth = temp.filter( city => city.Month === query.month )
 
+// const tempCity = cityMonth.filter( city => {
+//     if ( query.unit === 'celcius' ) {
+//         return Math.abs( Number( query.temperature ) - Number( city.HighC ) ) <= 3
 
+//     } else {
+//         return Math.abs( Number( query.temperature ) - Number( city.HighF ) ) <= 8
+//     }
+// } )
+
+// return tempCity.map( city => {
+//     if ( city.City === d.properties.name ) {
+//         return city
+//     }
+// } )
         // svg.selectAll( "circle" )
         //     .data( places_geojson.features )
         //     .enter()
